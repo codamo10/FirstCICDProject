fe6989d4b619fb3c00c6e1451d6c02c4d80ad295
codamo10 <codamo10@gmail.com>
2024-12-09 23:07:57 -0600

    commit 2

--- project.py
+++ project.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"message": "Mi primera vez usando GitHub Actions"}), 200
-
-@app.route('/autor')
-def about():
-    return jsonify({"autor": "Daniela Vega"}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def calculate_area_square(length: int | float) -> int | float:
+    """
+    Function to calculate the area of a square
+    :param length: length of the square
+    :return: area of the square
+    """
+    if not isinstance(length, (int, float)) or length <= 0:
+        raise TypeError("Length must be a positive non-zero number")
+    return length * length
