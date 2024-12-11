403b8acd6b60ad4c12cf2bff1d75e7a3a01a6d36
codamo10 <codamo10@gmail.com>
2024-12-10 22:46:02 -0600

    Mi primer workflow

--- src/project.py
+++ src/project.py
@@ -1,4 +1,4 @@
-def x(longitud):
+def get_area(longitud):
     if not isinstance(longitud, (int, float)) or longitud < 1:
         raise TypeError("Los valores deben ser enteros positivos")
     return longitud * longitud
--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -1,22 +1,22 @@
 import pytest
-from src.project import x
+from src.project import get_area
 
 def test_area_cuadrado():
-    assert x(3) == 9
-    assert x(4) == 16
-    assert x(8.4) == 70.56
+    assert get_area(3) == 9
+    assert get_area(4) == 16
+    assert get_area(8.4) == 70.56
     
 def test_area_cuadrado_string():
     with pytest.raises(TypeError):
-        x("56")
+        get_area("56")
 
 
 def test_area_cuadrado_list():
     with pytest.raises(TypeError):
-        x([43])
+        get_area([43])
 
 def test_area_cuadrado_negative():
     with pytest.raises(TypeError):
-        x(-7)
+        get_area(-7)
 
 
