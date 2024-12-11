0a987dc4128c6c4a2ef81c4132deac437b13efa9
codamo10 <codamo10@gmail.com>
2024-12-10 22:14:24 -0600

    Prueba workflow

--- src/project.py
+++ src/project.py
@@ -2,3 +2,4 @@ def cuadrado_area(longitud):
     if not isinstance(longitud, (int, float)) or longitud < 1:
         raise TypeError("Los valores deben ser enteros positivos")
     return longitud * longitud
+#Calcular area de un cuadrado
